65158948e6e936e97b7a7433fc98c5d0843bf943
Justin Williams <williams.justin.j@gmail.com>
2024-10-26 11:14:45 -0400

    project 5 updates

--- Project 5/Project5.R
+++ Project 5/Project5.R
@@ -1,3 +1,10 @@
+################################################################################
+#Project 5                                                                     #
+#Justin Williams                                                               #
+#                                                                              #
+#                                                                              #
+################################################################################
+
 #import libraries and data
 library(tidyverse)
 library(dplyr)
@@ -7,6 +14,67 @@ library(ggplot2)
 #load dataframe
 hrdata_df <- read.csv("/Users/justinwilliams/Code/9050advresearch/Project 5/HRData.csv")
 
-#initial analysis / plot
-scatterplot <- plot(hrdata_df$PerfScoreID, hrdata_df$Empsatisfaction, main="Performance Score vs Employee Satisfaction", xlab="Performance Score", ylab="Employee Satisfaction", pch=19)
-print (scatterplot)
+###initial analysis
+
+#summary statistics
+summary(hrdata_df$PerfScoreID)
+summary(hrdata_df$EmpSatisfaction)
+mode_perfscoreid <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+mode_perfscoreid_value <- mode_perfscoreid(hrdata_df$PerfScoreID)
+print(mode_perfscoreid_value)
+sd(hrdata_df$PerfScoreID)
+sd(hrdata_df$EmpSatisfaction)
+
+#mean centering
+
+#ggplot scatterplot
+ggplot(hrdata_df, aes(x=PerfScoreID, y=EmpSatisfaction)) + 
+    geom_point(color="orange", size=8) + 
+    labs(title="Performance Score vs Employee Satisfaction", x="Performance Score", y="Employee Satisfaction") + 
+    theme(panel.background = element_rect(fill = "white"),
+          axis.title.x = element_text(size=30),
+          axis.title.y = element_text(size=30),
+          axis.text.y = element_text(size=30),
+          axis.text.x = element_text(size=30),
+          plot.title = element_text(size=35))
+
+#adding regression and line
+geom_smooth(method = "lm", se = FALSE, fullrange=FALSE, level=0.95, color="purple")
+ggplot(hrdata_df, aes(x=PerfScoreID, y=EmpSatisfaction)) + 
+    geom_point() + 
+    geom_smooth(method = "lm", se = FALSE, fullrange=FALSE, level=0.95) + 
+    labs(title="Performance Score vs. Employee Satisfaction", x="Performance Score ID", y="Employee Satisfaction") + 
+    theme(panel.background = element_rect(fill = "white"),
+          axis.title.x = element_text(size=30),
+          axis.title.y = element_text(size=30),
+          axis.text.y = element_text(size=30),
+          axis.text.x = element_text(size=30),
+          plot.title = element_text(size=35))
+
+#fit the linear regression model
+model <- lm(EmpSatisfaction ~ PerfScoreID, data = hrdata_df)
+
+#model summary
+model_summary <- summary(model)
+
+#model results data frame
+results_df <- data.frame(
+Variance = model_summary$sigma^2,
+Beta = coef(model_summary)[, "Estimate"],
+Std_Error = coef(model_summary)[, "Std. Error"],
+R = sqrt(model_summary$r.squared),
+R_Squared = model_summary$r.squared,
+P_value = coef(model_summary)[, "Pr(>|t|)"],
+F = model_summary$fstatistic[1]
+)
+
+#print results
+print("Results Data Frame:")
+print(results_df)
+
+#model fit measures
+#moel comparisons
+#model specific results
